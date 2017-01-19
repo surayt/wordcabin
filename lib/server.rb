3c75e6583256e06f348de7870dd5eb45a8fbc9f9
J. R. Schmid <jrs+git@weitnahbei.de>
2017-01-19 17:38:03 +0100

    Rendering appropriate HTML file.

--- lib/server.rb
+++ lib/server.rb
@@ -21,11 +21,7 @@ module Textbookr
       I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
       I18n.load_path = Dir[(ROOT+'locales'+'*.yml').expand_path]
       I18n.backend.load_translations
-      # use Rack::Locale
-      # http://stackoverflow.com/questions/3104658/how-to-detect-language-from-url-in-sinatra
-      # set :locales, CONFIG[:locales]
-      # set :default_locale, 'en'
-      # set :locale_pattern, /^\/?(#{Regexp.union(settings.locales)})(\/.+)$/
+      # use Rack::Locale # TODO: Fix this so that it does not inject region as well!
       # CSS compiler
       Sass::Plugin.options[:style] = :compressed
       Sass::Plugin.options[:cache_location] = (Pathname(CONFIG[:cache_path])+'sass').expand_path.to_s
@@ -34,40 +30,34 @@ module Textbookr
       use Sass::Plugin::Rack
     end
 
-    def default_chapter(locale)
-      dirs_in_locale = Pathname(CONFIG[:data_path])+'chapters'+'*'+locale
-      puts dirs_in_locale
-      #puts Dir.glob(dirs_in_locale).inspect
-    end
-
-=begin
-    helpers do
-      def locale
-        @locale || settings.default_locale
-      end
-    end
-
-    before do
-      @locale, request.path_info = $1, $2 if request.path_info =~ settings.locale_pattern
-    end
-=end
-
+    # Prepending the rest of the route with the locale code
     before '/:locale/?*' do
       I18n.locale = params[:locale]
       request.path_info = '/'+params[:splat].first
     end
 
+    # Landung page showing the list of available L1s
     get '/' do
       @locales = I18n.available_locales
       haml :list_languages
     end
 
+    # Displaying the contents themselves
     get '/*' do |splat|
       @locale = I18n.locale
       cefr_level, chapter, heading = splat.split("/")
       params[:cefr_level] = cefr_level || 'a1'
       params[:chapter]    = chapter    || 'intro'
       params[:heading]    = heading    || '1'
+      content_file = Pathname(CONFIG[:cache_path]) +
+                     'chapters'                    +
+                     @locale.to_s                  +
+                     "#{params[:cefr_level]}-#{params[:chapter]}.html"
+      @contents = begin
+        File.read(content_file)
+      rescue
+        I18n.t(:no_contents)
+      end
       haml :show_contents
     end
   end
