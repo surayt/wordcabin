fed7b99ffda1edfc7ecb74eee09d962bb29fab94
J. R. Schmid <jrs+git@weitnahbei.de>
2017-02-06 14:43:25 +0000

    Explanatory commentc

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-  // Only show the first top level item's children when page opens
+  // Only show the first top level item's children when page opens.
   collapse_top_level_items();
   $('nav#sidebar li.level_1:first-child > ul').show()
   
-  // Also, disable the top level links (we only want to use them to expand the lower levels)
+  // Disable the top level links and make them open their parents' child <ul>.
+  // An advantage of doing things in the below order is that clicking an 
+  // already expanded section won't close it but just keep it open, which
+  // I find preferable from a UI perspective.
   $('nav#sidebar li.level_1 > a').click(function(e) {
     e.preventDefault();
     collapse_top_level_items();
