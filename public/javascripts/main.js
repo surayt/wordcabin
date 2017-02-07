f8932c89d7679e8601a9e896f1b6d7703ec85ead
J. R. Schmid <jrs+git@weitnahbei.de>
2017-02-07 12:53:04 +0000

    Saving and restoring TOC scrollbar position.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  // Restore TOC scrollbar position from cookie, if present
+  restore_toc_scrollbar_position();
+
   // Only show the first top level item's children when page opens.
   collapse_top_level_items();
   $('nav#sidebar li.level_1:first-child > ul').show()
@@ -12,8 +15,19 @@ $(document).ready(function() {
     collapse_top_level_items();
     $(this).parent().children('ul').toggle();
   });
+  
+  // Cause second and third level links to save the TOC scrollbar position
+  // to a cookie so it may be restored upon the page being loaded again.
+  $('nav#sidebar li.level_2 a').click(function(e) {
+    Cookies.set('textbookr_toc_scrollbar_position', $('nav#sidebar').scrollTop());    
+  });
 });
 
 function collapse_top_level_items() {
   $('nav#sidebar li.level_1 > ul').hide();
+}
+
+function restore_toc_scrollbar_position() {
+  pos = Cookies.get('textbookr_toc_scrollbar_position')
+  $('nav#sidebar').scrollTop(pos || 0);
 }
