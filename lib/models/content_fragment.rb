dbfaac63fbf554c73331a3c856a9e686ef6d884a
J. R. Schmid <jrs+git@weitnahbei.de>
2017-06-26 22:00:31 +0200

    Finer validations.

--- lib/models/content_fragment.rb
+++ lib/models/content_fragment.rb
@@ -3,9 +3,9 @@ require 'sanitize'
 module SinatraApp
   class ContentFragment < ActiveRecord::Base
     # TODO: i18n!
-    validates :book, presence: {message: 'must be present, even when chapter is empty'}
-    validates :locale, presence: {message: 'must and should be present'}, length: {is: 2, message: 'must be in ISO 3166-1 Alpha 2 encoding'}
-    validates :chapter, uniqueness: {scope: [:locale, :book], message: 'must be unique within book and locale'}
+    validates :book, presence: {message: 'must be present, even when chapter is empty.'}
+    validates :locale, presence: {message: 'must and should be present'}, length: {is: 2, message: 'must be in ISO 3166-1 Alpha 2 encoding.'}
+    validates :chapter, uniqueness: {scope: [:locale, :book], message: 'must be unique within book and locale.'}
     validates :chapter, format: {with: /^[\d+.]*\d+$/, multiline: true, message: 'must be in a format like 2.3.4.5, etc.'}, allow_blank: true
   
     def path
