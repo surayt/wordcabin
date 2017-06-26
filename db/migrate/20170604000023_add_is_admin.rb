0b263182e69b5ea5bf11484d0996558fb8379080
J. R. Schmid <jrs+git@weitnahbei.de>
2017-06-26 21:59:58 +0200

    For future reference.

--- db/migrate/20170604000023_add_is_admin.rb
+++ db/migrate/20170604000023_add_is_admin.rb
@@ -2,4 +2,5 @@ class AddIsAdmin < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :is_admin, :boolean, default: false
   end
+  # User.create(email: 'admin@surayt.com', password: '123123', password_confirmation: '123123', is_admin: true)
 end
