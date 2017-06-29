029024921fea14540e7b8b74c17e24987d05b5af
J. R. Schmid <jrs+git@weitnahbei.de>
2017-06-29 13:10:13 +0200

    Updating gems, adding jquery and js-cookie from their official repos.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,5 @@
-//= require_directory ./jslibs
+//= require ./jquery/dist/jquery
+//= require ./js-cookie/src/js.cookie
 //= require_directory ./tinymce_plugins
 //= require ./jquery-ujs/src/rails
 //= require_directory ./application
