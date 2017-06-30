0c36091bb4d8bd8f1ff77f5ca0f818cd84ac57c8
J. R. Schmid <jrs+git@weitnahbei.de>
2017-06-30 04:53:36 +0200

    Including jscroll.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 //= require ./jquery/dist/jquery
 //= require ./js-cookie/src/js.cookie
+//= require ./jscroll/jquery.jscroll
 //= require_directory ./tinymce_plugins
 //= require ./jquery-ujs/src/rails
 //= require_directory ./application
