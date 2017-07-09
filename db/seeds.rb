573c162d3ea6aa9a015d68ac1a027234ca5f81ed
J. R. Schmid <jrs+git@weitnahbei.de>
2017-07-09 14:20:48 +0200

    Bugfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(email: 'admin@surayt.com', password: '123123', password_confirmation: '123123')
+User.create(email: 'admin@surayt.com', password: '123123', password_confirmation: '123123', is_admin: true)
