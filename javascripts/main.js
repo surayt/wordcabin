e8c369891c42d19fb8263f788909d3050e778d8f
J. R. Schmid <jrs+git@weitnahbei.de>
2017-07-20 19:25:52 +0200

    Adding new library to help with debugging

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,8 @@
+//= require ./underscore/underscore
 //= require ./jquery-dist/dist/jquery
 //= require ./js-cookie/src/js.cookie
 //= require_directory ./tinymce_plugins
 //= require ./jquery-ujs/src/rails
 //=# require ./infinite-scroll/dist/infinite-scroll.pkgd
-//= require ./js-url/url.js
+//= require ./js-url/url
 //= require_directory ./application
