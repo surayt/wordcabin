bb6accabaca3cfd3f6e4d69378ad786590882514
J. R. Schmid <jrs+git@weitnahbei.de>
2017-07-21 03:53:30 +0200

    Fixing ContentFragment validations, adding tests
    Closing #27.

--- lib/models/content_fragment.rb
+++ lib/models/content_fragment.rb
@@ -7,28 +7,64 @@ module SinatraApp
     before_save :fill_sorting_column
 
     # TODO: i18n!
+    # CHECK: tests written!
     validates :book, presence: {message: 'must be present, even when chapter is empty.'}
-    validates :locale, presence: {message: 'must and should be present'}, length: {is: 2, message: 'must be in ISO 3166-1 Alpha 2 encoding.'}
-    # validates :chapter, uniqueness: {scope: [:locale, :book], message: 'must be unique within book and locale.'}
-    validates :chapter, format: {with: /^[\d+.]*\d+$/, multiline: true, message: 'must be in a format like 2.10.4.5, etc.'}, allow_blank: true # TODO: allow_nil, even?
-    # TODO: check whether:
-    # - new element would, given its chapter string, have a parent?
-    # validate :ensure_chapter_has_parent # TODO: Disabling, because it still has problems.
-    # - element to be deleted has children that need to be deleted?
-    before_destroy :chapter_has_no_children
+    validates :locale, presence: {message: 'must and should be present'}, length: {is: 2, message: 'must be in ISO 3166-1 Alpha 2 encoding.'}    
+    validates :chapter, format: {with: /^[\d+.]*\d+$/, multiline: true, message: 'must be in a format like 2.10.4.5, etc.'}, allow_blank: true
+
+    # TODO: i18n!
+    # CHECK: tests written!
+    validate :ensure_chapter_is_unique
+    def ensure_chapter_is_unique
+      unique = !(ContentFragment.chapters(locale, book).map {|existing_fragment| existing_fragment.chapter}.include? chapter)
+      errors.add(:chapter, 'already exists in this book for the selected language version.') unless unique
+    end
     
-#    def ensure_chapter_has_parent
-#      if chapter.blank? && !ContentFragment.book(locale, book).any?
-#        errors.add(:chapter, 'can only be specified together with an already existing book! To create a new book, leave empty.')
-#      end
-#    end
-    
-    def ensure_chapter_has_no_children
-      if chapter.blank? && ContentFragment.where(locale: locale, book: book).non_empty_chapters.any?
-        errors.add(:base, "Can't delete book that still has any children!")
-        return false
+    # TODO: i18n!
+    # CHECK: tests written!
+    validate :ensure_chapter_is_book_or_has_parent
+    def ensure_chapter_is_book_or_has_parent
+      if chapter.blank?
+        # It's a book!
+        unique = !(ContentFragment.books(locale).map {|existing_fragment| existing_fragment.book}.include? book)
+        errors.add(:book, 'already exists for the selected language version.') unless unique
+      else
+        # It's not a book!
+        has_parent = ContentFragment.book(locale, book).any?
+        errors.add(:book, 'must already exist or otherwise the new content fragment will not be visible.') unless has_parent
       end
-      return true
+    end
+    
+    before_destroy :ensure_chapter_has_no_children
+    def ensure_chapter_has_no_children
+      # This one is non-trivial. It goes like this:
+      # - are we a book?
+      # - if so:
+      #   - there can't be anything having our book name 
+      #     AND locale
+      # - if we're not a book:
+      #   - nothing can have our book name
+      #     AND locale
+      #     AND a chapter with at least one more segment than we do where the segments leading up to the extra segment(s) is/are the same as our chapter's segments
+      if chapter.blank?
+        # It's a book!
+        has_no_children = !ContentFragment.chapters(locale, book).any?
+      else
+        # It's not a book!
+        has_no_children = true
+        ContentFragment.where("chapter LIKE ?", "#{chapter}%").chapters(locale, book).each do |possible_child|
+          if chapter.split('.').count < possible_child.chapter.split('.').count
+            has_no_children = false 
+            break
+          end
+        end
+      end
+      # Not an ordinary validation callback, thus need to throw(:abort).
+      # https://stackoverflow.com/questions/123078/how-do-i-validate-on-destroy-in-rails
+      unless has_no_children
+        errors.add(:base, "Can't delete book that still has any children!")
+        throw(:abort)
+      end
     end
   
     def path
@@ -76,8 +112,11 @@ module SinatraApp
       fragments.any? ? fragments.first : ContentFragment.new(locale: locale, book: book, chapter: next_chapter)
     end
 
+    # Meant for private use, but we'll see...
     scope :non_empty_chapters, -> { where("chapter <> ''") }
     scope :empty_chapter, -> { where(chapter: [nil, '']) }
+    # Meant for public use...
+    scope :books, ->(locale) { where(locale: locale).empty_chapter }
     scope :book, ->(locale, book) { where(locale: locale, book: book).empty_chapter }
     scope :chapters, ->(locale, book) { where(locale: locale, book: book).non_empty_chapters.uniq }
     scope :chapter, ->(locale, book, chapter) { where(locale: locale, book: book, chapter: chapter) }
--- lib/tests/tests.rb
+++ lib/tests/tests.rb
@@ -2,6 +2,7 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
 require 'pathname'
+require 'simple-random'
 MAIN_CONFIG = Pathname('../../config')+'config.rb'
 require_relative MAIN_CONFIG
 require_relative Config.lib+'server.rb'
@@ -14,14 +15,88 @@ module SinatraApp
       Server
     end
     
+    def some_integer
+      @rand.uniform(2,9999999).to_i
+    end
+    
+    def some_book
+      "book#{some_integer}"
+    end
+    
+    def some_chapter
+      segments = @rand.uniform(1, 10).to_i
+      chapter = ''
+      segments.times {chapter << "#{@rand.uniform(1,999).to_i}."}
+      chapter.chomp '.'
+    end
+    
+    def some_locale
+      locales = [:de, :en, :tr, :ar, :sv, :nl, :fr]
+      locales.sample
+    end
+    
+    def setup
+      ContentFragment.delete_all
+      @book1 = ContentFragment.create(locale: :en, book: 'book1', chapter: nil)
+      @rand = SimpleRandom.new
+      @rand.set_seed
+    end
+    
     def test_can_create_valid_content_fragment
-      c = ContentFragment.new(locale: :de, book: 'Test')
+      c = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: some_chapter)
       assert c.save
     end
     
     def test_fails_when_creating_content_fragment_without_locale
-      c = ContentFragment.new(locale: nil, book: 'Test')
+      c = ContentFragment.new(locale: nil, book: some_book)
       assert c.save == false
+    end
+    
+    def test_fails_when_creating_content_fragment_without_book
+      c = ContentFragment.new(locale: some_locale, book: nil)
+      assert c.save == false
+    end
+    
+    def test_fails_when_creating_content_fragment_with_subtly_illegal_chapter_format
+      c = ContentFragment.new(locale: some_locale, book: some_book, chapter: '1.2.3,4')
+      assert c.save == false
+    end
+    
+    def test_fails_when_creating_content_fragment_with_chapter_that_already_exists_in_the_scope_of_locale_and_book
+      chapter = some_chapter
+      c1 = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: chapter)
+      c2 = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: chapter)
+      assert c1.save == true
+      assert c2.save == false
+    end
+    
+    def test_its_possible_to_create_two_content_fragments_with_consecutive_chapter_numbers
+      c1 = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: '1.2.3.4')
+      c2 = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: '1.2.3.5')
+      assert c1.save == true
+      assert c2.save == true
+    end
+    
+    def test_its_possible_to_delete_a_fragment_without_children
+      c = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: some_chapter)
+      assert c.save == true
+      c.destroy
+      assert c.errors.any? == false
+    end
+    
+    def test_its_impossible_to_delete_a_fragment_with_children
+      parent = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: '10')
+      sibling = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: '11')
+      child = ContentFragment.new(locale: @book1.locale, book: @book1.book, chapter: '10.5')
+      assert parent.save == true
+      assert sibling.save == true
+      assert child.save == true
+      initial_count = ContentFragment.count
+      parent.destroy
+      assert_equal initial_count, ContentFragment.count
+      child.destroy
+      parent.destroy
+      assert_equal initial_count - 2, ContentFragment.count
     end
   end
 end
