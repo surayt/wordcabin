a0fbe6e1600fa9a218b0137e122d39661d48ac4f
J. R. Schmid <jrs+git@weitnahbei.de>
2017-08-15 23:33:59 +0200

    Only show books that are is_published == true.

--- lib/routes.rb
+++ lib/routes.rb
@@ -25,7 +25,9 @@ module Wordcabin
     # Landing page showing the list of available L1s.
     
     get '/' do
-      @fragment_sets = ContentFragment.empty_chapters.group_by(&:book)
+      @fragment_sets = ContentFragment.empty_chapters
+      @fragment_sets = @fragment_sets.published unless current_user.is_admin?
+      @fragment_sets = @fragment_sets.group_by(&:book)
       if !@fragment_sets.any? && current_user.is_admin?
         redirect to("/#{locale}/new?view_mode=edit")
       else
