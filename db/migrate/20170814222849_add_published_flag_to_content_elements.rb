855c503b65e60b0a920a1a2c89e58fddad75350a
J. R. Schmid <jrs+git@weitnahbei.de>
2017-08-17 22:59:45 +0200

    By default, setting first book to is_published=true for English and German.

--- db/migrate/20170814222849_add_published_flag_to_content_elements.rb
+++ db/migrate/20170814222849_add_published_flag_to_content_elements.rb
@@ -1,5 +1,8 @@
 class AddPublishedFlagToContentElements < ActiveRecord::Migration[5.1]
   def change
     add_column :content_fragments, :is_published, :boolean, default: false
+    [:en, :de].each do |locale|
+      ContentFragment.books(locale).first.update_attribute(:is_published, true)
+    end
   end
 end
