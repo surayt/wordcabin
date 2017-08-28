3d11982d9bd4721b1e2d8bde7600f5974af13d38
J. R. Schmid <jrs+git@weitnahbei.de>
2017-08-28 04:41:14 +0200

    Adding ltr/rtl handling to Cloze_Exercise.
    These changes should conclude #139 for the time being.

--- lib/models/text_fragment.rb
+++ lib/models/text_fragment.rb
@@ -9,6 +9,18 @@ module Wordcabin
     belongs_to :question, foreign_key: :question_id
     # for question
     has_many :answers
+    
+    def text
+      unless text_ltr.nil? || text_ltr.strip.blank? || text_rtl.nil? || text_rtl.strip.blank?
+        s = [text_ltr, text_rtl].join(' ') 
+      end
+    end
+
+    def key
+      unless key_ltr.nil? || key_ltr.strip.blank? || key_rtl.nil? || key_rtl.strip.blank?
+        s = [key_ltr, key_rtl].join(' ')
+      end
+    end
   end
   
   class Question < TextFragment
