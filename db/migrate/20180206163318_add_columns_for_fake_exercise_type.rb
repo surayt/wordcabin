96ca3bae096be120277138b4fbccd2d82566ea15
J. R. Schmid <jrs+git@weitnahbei.de>
2018-02-21 18:41:04 +0100

    DB col for "fake" exercise type.

---
+++ db/migrate/20180206163318_add_columns_for_fake_exercise_type.rb
@@ -0,0 +1,7 @@
+class AddColumnsForFakeExerciseType < ActiveRecord::Migration[5.1]
+  def change
+    add_column :exercises, :content_fragment_id, :integer, null: true
+    add_column :exercises, :sort_order, :integer, null: true
+    add_column :exercises, :html, :text, null: true
+  end
+end
