287f7993c33143c9b59460c0c5e0abc745c029f2
J. R. Schmid <jrs+git@weitnahbei.de>
2018-03-04 01:51:24 +0100

    db:migrate complete

--- db/migrate/20180206163318_add_columns_for_fake_exercise_type.rb
+++ db/migrate/20180206163318_add_columns_for_fake_exercise_type.rb
@@ -3,6 +3,5 @@ class AddColumnsForFakeExerciseType < ActiveRecord::Migration[5.1]
     add_column :exercises, :content_fragment_id, :integer, null: true
     add_column :exercises, :sort_order, :integer, null: true
     add_column :exercises, :html, :text, null: true
-    Exercise.where(locale: nil).update_attribute(:locale, :en)
   end
 end
